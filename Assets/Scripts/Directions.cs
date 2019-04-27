e6408593c1182e9c271b382f68f60d49a9b85591
Roberto Paredes <rdparedessalazar@gmail.com>
2019-04-27 00:30:55 -0500

    Add player object & playerController

---
+++ Assets/Scripts/Directions.cs
@@ -0,0 +1,7 @@
+﻿public enum Directions
+{
+    Up = 1,
+    Right = 2,
+    Down = 3,
+    Left = 4
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public bool isWalking;
+
+    internal Directions facingDirection;          // used to determine which animation to play
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        HandleActionKeys();
+        HandleMovement();
+    }
+
+    private void HandleActionKeys()
+    {
+        // TODO
+    }
+
+    private void HandleMovement()
+    {        
+        var direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.up;
+            if (
+                !(Input.GetKey(KeyCode.S)
+                || Input.GetKey(KeyCode.A)
+                || Input.GetKey(KeyCode.D))
+             )
+            {
+                facingDirection = Directions.Up;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+            if (
+                !(Input.GetKey(KeyCode.W)
+                || Input.GetKey(KeyCode.A)
+                || Input.GetKey(KeyCode.S))
+             )
+            {
+                facingDirection = Directions.Right;
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.down;
+            if (
+                !(Input.GetKey(KeyCode.W)
+                || Input.GetKey(KeyCode.A)
+                || Input.GetKey(KeyCode.D))
+             )
+            {
+                facingDirection = Directions.Down;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+            if (
+                !(Input.GetKey(KeyCode.W)
+                || Input.GetKey(KeyCode.S)
+                || Input.GetKey(KeyCode.D))
+             )
+            {
+                facingDirection = Directions.Left;
+            }
+        }
+
+        isWalking = direction.magnitude > 0;
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+}
