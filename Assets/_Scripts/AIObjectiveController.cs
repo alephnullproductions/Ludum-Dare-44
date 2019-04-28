7791624626a9a734eacdf76956e402d06c16d3f6
Mosher <polyblank2@gmail.com>
2019-04-27 21:00:47 -0700

    Added Some Basic Scripts
    Player Interact Script
    Waypoint Script for Persons (Vampires and Humans)
    ScriptableObject Data Objects

---
+++ Assets/_Scripts/AIObjectiveController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIObjectiveController : MonoBehaviour {
+
+    public GameObject targetZone;
+
+    public List<GameObject> zlist = new List<GameObject>();
+    [SerializeField]
+    public Queue<GameObject> Zones = new Queue<GameObject>();
+    // Zones need a container object with wait duration...
+
+    [SerializeField]
+    private float dist = 100;
+    private NavMeshAgent nav;
+    
+    private bool atZone = false;
+    private bool isTraveling = false;
+    
+    private void Awake() {
+        Zones = new Queue<GameObject>(zlist);
+        nav = GetComponent<NavMeshAgent>();
+
+        targetZone = Zones.Dequeue();
+        atZone = false;
+        dist = Vector3.Distance(transform.position,
+            targetZone.transform.position);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(targetZone.name);
+        
+        dist = Vector3.Distance(transform.position,
+            targetZone.transform.position);
+
+        // Get Destination
+        if (atZone) {
+            targetZone = Zones.Dequeue();
+            atZone = false;
+        }
+
+        if (!atZone && !isTraveling) {
+            nav.SetDestination (targetZone.transform.position);
+            dist = Vector3.Distance(transform.position,
+                targetZone.transform.position);
+            isTraveling = true;
+        }
+
+        // set targetZone as destination
+        if (isTraveling) {
+
+            if (dist < 2 && isTraveling) {
+                // Trigger wait
+                // If wait is up : Trigger move
+                atZone = true;
+                isTraveling = false;
+            }
+        }
+        
+        // Return Destination to the end
+        if (atZone) {
+            Zones.Enqueue(targetZone);
+            isTraveling = false;
+        }
+        
+    }
+}
---
+++ Assets/_Scripts/ScriptableObjects/Zone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Zone", menuName = "Zone")]
+public class Zone : ScriptableObject
+{
+    // These are Waypoints
+    public string title; // such as someone's Desk
+    
+    // Example:
+    // Kitchen takes 10 seconds because people eat there
+    public string purposeOfZone;
+
+    public float humanActivityDuration;
+
+    public GameObject relatedObject;
+    public GameObject relatedPerson;
+    
+}
