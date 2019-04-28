df5097a8feea306ab1a0b3ddd979976f7917cb13
Aleph Null Productions <alephnullproductions@gmail.com>
2019-04-28 12:57:47 -0700

    More Office Work

---
+++ Assets/CelingManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelingManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject celing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        celing.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
