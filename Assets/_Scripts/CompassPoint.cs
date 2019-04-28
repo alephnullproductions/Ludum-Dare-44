4d78256c717c0595f31d09aa5dee78d1a199693c
Mosher <polyblank2@gmail.com>
2019-04-28 13:36:54 -0700

    Added Compass and Character Schedule Demo
    Compass points at objective

---
+++ Assets/_Scripts/CompassPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassPoint : MonoBehaviour {
+
+    public List<GameObject> gameTasks;
+    public GameObject currentTask;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(currentTask.transform);
+    }
+}
