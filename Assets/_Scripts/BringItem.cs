fafcd37ba5058f37765a8b5ef181b139b5509975
Mosher <polyblank2@gmail.com>
2019-04-28 11:59:31 -0700

    Picking Up Objects doesn't obscure vision
    Picking Up Objects to side
    Event and Trigger System starting to take shape

--- Assets/_Scripts/BringItem.cs
+++ Assets/_Scripts/BringItem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using GameEvents;
 using UnityEngine;
 
 public class BringItem : MonoBehaviour
@@ -16,17 +17,21 @@ public class BringItem : MonoBehaviour
         
     }
 
+    public GameEvent OnDelivery;
+
     private void OnTriggerEnter(Collider other) {
         // add a stipulation in here like: expects coffee
         if (other.transform.tag == "Coffee") {
             //Trigger Event
             Debug.Log("Coffee Delivered");
+            OnDelivery.Raise();
             Destroy(other.gameObject);
         }
         
         if (other.transform.tag == "Paper") {
             //Trigger Event
             Debug.Log("Paper Delivered");
+            OnDelivery.Raise();
             Destroy(other.gameObject);
         }
     }
